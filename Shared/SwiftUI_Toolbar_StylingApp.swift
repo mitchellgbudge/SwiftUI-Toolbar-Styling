7f1fa04e3314c615e675be72300ca2994f99ce4f
Mitchell <mitchellbudge@gmail.com>
2020-08-21 18:15:09 -0600

    Initial Commit

---
+++ Shared/SwiftUI_Toolbar_StylingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Toolbar_StylingApp.swift
+//  Shared
+//
+//  Created by Mitchell Budge on 8/21/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Toolbar_StylingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
