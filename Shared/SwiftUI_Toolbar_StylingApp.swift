3c33baf3496240ca463449cdf807b21751434a64
Mitchell <mitchellbudge@gmail.com>
2020-08-21 18:21:41 -0600

    Stage base project

--- Shared/SwiftUI_Toolbar_StylingApp.swift
+++ Shared/SwiftUI_Toolbar_StylingApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_Toolbar_StylingApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ToolbarPlacementListView()
         }
     }
 }
--- Shared/ToolbarPlacementListView.swift
+++ Shared/ToolbarPlacementListView.swift
@@ -7,15 +7,16 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ToolbarPlacementListView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Text("")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ToolbarPlacementListView()
     }
 }
